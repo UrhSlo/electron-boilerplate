1de6feb25b1e0f64c13dee738d9f38a1879e8c9a
JanezSedeljsak <43420276+JanezSedeljsak@users.noreply.github.com>
2019-10-06 00:48:29 +0200

    initial commit

---
+++ app/index.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron')
+
+require('electron-debug')({
+    showDevTools: process.env.NODE_ENV === 'development'
+})
+
+function createWindow() {
+    let win = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadFile('./app/components/home.html')
+}
+
+app.on('ready', createWindow)
